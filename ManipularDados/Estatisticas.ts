8e200a21538411dba6eec5149f4b2cfce6f862f4
Danilo <danilo.nascimento2@hotmail.com>
2023-08-01 19:15:25 -0300

    Criando countBy para trazer quantidade de pagamentos e status

--- ManipularDados/Estatisticas.ts
+++ ManipularDados/Estatisticas.ts
@@ -1,4 +1,5 @@
-import { Transacao } from "./normalizarTransacao";
+import countBy from "./countBy.js";
+import { Transacao } from "./normalizarTransacao.js";
 
 type TransacaoValor = Transacao & { valor: number };
 
@@ -10,13 +11,26 @@ function filtrarValor(transacao:
 export default class Estatisticas {
     private transacoes;
     total;
+    pagamento;
+    status;
     constructor(transacoes: Transacao[]) {
         this.transacoes = transacoes;
         this.total = this.setTotal()
+        this.pagamento = this.setPagamento();
+        this.status = this.setStatus();
     }
+
     private setTotal() {
         return this.transacoes.filter(filtrarValor).reduce((acc, item) => {
             return acc + item.valor
         }, 0)
+    }
+
+    private setPagamento() {
+        return countBy(this.transacoes.map(({pagamento}) => pagamento))
+    }
+
+    private setStatus() {
+        return countBy(this.transacoes.map(({ status }) =>  status ))
     }
 }
