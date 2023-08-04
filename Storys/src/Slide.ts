824e7be6178ea20ce50c6000dce415347e2cf22f
Danilo <danilo.nascimento2@hotmail.com>
2023-08-04 15:47:19 -0300

    Criando Pause para videos

--- Storys/src/Slide.ts
+++ Storys/src/Slide.ts
@@ -21,7 +21,9 @@ export default class Slide {
 		this.controls = controls;
 		this.time = time;
 
-		this.index = 0;
+		this.index = localStorage.getItem("activeSlide")
+			? Number(localStorage.getItem("activeSlide"))
+			: 0;
 		this.slide = this.slides[this.index];
 
 		this.timeout = null;
@@ -35,13 +37,31 @@ export default class Slide {
 
 	hide(el: Element) {
 		el.classList.remove("active");
+		if (el instanceof HTMLVideoElement) {
+			el.currentTime = 0;
+			el.pause();
+		}
 	}
 	show(index: number) {
 		this.index = index;
 		this.slide = this.slides[this.index];
+		localStorage.setItem("activeSlide", String(this.index));
 		this.slides.forEach((el) => this.hide(el));
 		this.slide.classList.add("active");
-		this.auto(this.time);
+		if (this.slide instanceof HTMLVideoElement) {
+			this.autoVideo(this.slide);
+		} else {
+			this.auto(this.time);
+		}
+	}
+	autoVideo(video: HTMLVideoElement) {
+		video.muted = true;
+		video.play();
+		let firstPlay = true;
+		video.addEventListener("playing", () => {
+			if (firstPlay) this.auto(video.duration * 1000);
+			firstPlay = false;
+		});
 	}
 
 	auto(time: number) {
@@ -66,6 +86,7 @@ export default class Slide {
 		this.pausedTimeout = new Timeout(() => {
 			this.timeout?.pause();
 			this.paused = true;
+			if (this.slide instanceof HTMLVideoElement) this.slide.pause();
 		}, 300);
 	}
 
@@ -74,6 +95,7 @@ export default class Slide {
 		if (this.paused) {
 			this.paused = false;
 			this.timeout?.continue();
+			if (this.slide instanceof HTMLVideoElement) this.slide.play();
 		}
 	}
 
